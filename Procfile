9235d5d80e7f7641636e468f08a975e0fe04b9f4
Mister-0-Zero <alexyulyadkov@gmail.com>
2025-06-27 03:02:09 +0300

    fixing errors with downloading and connecting

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: uvicorn bot:app_web --host 0.0.0.0 --port 8080
--- bot.py
+++ bot.py
@@ -4,7 +4,9 @@ import asyncio
 from fastapi import FastAPI, Request
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from dotenv import load_dotenv
 
+load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
