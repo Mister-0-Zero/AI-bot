31662420f497a6a3b0c1af1c655512402842c3e5
Mister-0-Zero <alexyulyadkov@gmail.com>
2025-06-28 21:08:53 +0300

    check possiable connect

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -7,10 +7,25 @@ from app.core.db import init_db
 from app.telegram.bot import app_tg
 from app.telegram.handlers import register_handlers
 from app.core.logging_config import get_logger
+import httpx
 
 logger = get_logger(__name__)
 WEBHOOK_PATH = "/telegram-webhook"
 WEBHOOK_URL = f"https://{RAILWAY_DOMAIN}{WEBHOOK_PATH}" if RAILWAY_DOMAIN else None
+
+
+async def wait_for_webhook_ready(url: str, timeout: int = 10):
+    for i in range(timeout):
+        try:
+            async with httpx.AsyncClient() as client:
+                r = await client.get(url)
+                if r.status_code == 405 or r.status_code == 200:
+                    return
+        except Exception:
+            pass
+        await asyncio.sleep(1)
+    raise RuntimeError("Webhook URL не стал доступен за разумное время.")
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -27,7 +42,7 @@ async def lifespan(app: FastAPI):
         threading.Thread(target=_run, daemon=True).start()
     else:
         await app_tg.initialize()
-        await asyncio.sleep(2)  # << это нужно добавить
+        await wait_for_webhook_ready(WEBHOOK_URL)  # ← ждём, пока Railway поднимет URL
         logger.info(f"Установка webhook: {WEBHOOK_URL}")
         await app_tg.bot.set_webhook(url=WEBHOOK_URL)
 
