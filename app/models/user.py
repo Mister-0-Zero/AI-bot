fc69f891f24e1bb96321a0bb418a613f09bfe35f
Mister-0-Zero <alexyulyadkov@gmail.com>
2025-06-29 01:34:06 +0300

    fix: Table User, field: token_expiry

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,10 @@
 from typing import Optional
-from datetime import datetime
 from sqlmodel import SQLModel, Field
+from datetime import datetime, timezone
 
 class User(SQLModel, table=True):
     telegram_id: int = Field(primary_key=True)
     email: Optional[str] = None
     access_token: Optional[str] = None
     refresh_token: Optional[str] = None
-    token_expiry: Optional[datetime] = None
+    token_expiry: Optional[datetime] = Field(default_factory=lambda: datetime.now(timezone.utc))
