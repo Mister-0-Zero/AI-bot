e4e2e88c4da733d11c153bf03d091b8997fe5c56
Mister-0-Zero <alexyulyadkov@gmail.com>
2025-06-28 21:01:35 +0300

    added a delay so that railway can connect services and dependencies before installing webhook_url.

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -27,6 +27,7 @@ async def lifespan(app: FastAPI):
         threading.Thread(target=_run, daemon=True).start()
     else:
         await app_tg.initialize()
+        await asyncio.sleep(2)  # << это нужно добавить
         logger.info(f"Установка webhook: {WEBHOOK_URL}")
         await app_tg.bot.set_webhook(url=WEBHOOK_URL)
 
