7f2ce80c9de9b59af0a36dcf29f85997d14ecf7b
Mister-0-Zero <alexyulyadkov@gmail.com>
2025-07-17 17:13:41 +0300

    increased response time

--- app/telegram/bot.py
+++ app/telegram/bot.py
@@ -1,9 +1,18 @@
 from telegram.ext import ApplicationBuilder
+from telegram.request import HTTPXRequest
 
 from app.core.config import BOT_TOKEN
 from app.core.logging_config import get_logger
 
 logger = get_logger(__name__)
 
-app_tg = ApplicationBuilder().token(BOT_TOKEN).build()
+request = HTTPXRequest(
+    connect_timeout=90.0,
+    read_timeout=90.0,
+    write_timeout=90.0,
+    pool_timeout=90.0,
+)
+
+app_tg = ApplicationBuilder().token(BOT_TOKEN).request(request).build()
+
 logger.info("Telegram Application initialized")
